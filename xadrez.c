af424217693cc92597d447b0b98e8351d6cd334b
daniel-chaud <danielchaud2022@gmail.com>
2026-04-13 22:54:58 +0000

    Desafio novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,180 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int i = 0, numero, numero2, numero3;
+    unsigned int escolhadirecao, escolhadirecao2, escolhadirecao3;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("escolha a direção que deseja ir com a torre!: \n");
+    printf("1. Esquerda\n");
+    printf("2. Direita\n");
+    printf("3. Cima\n");
+    printf("Para qual direção você  vai ?: \n");
+    scanf("%d", &escolhadirecao);
+    
+    switch (escolhadirecao)
+    {
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    case 1:
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero);
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+       for (i; i <= numero - 1; i++)
+       {
+        printf("Andando 1 casa para a Esquerda\n");
+       }
+       
+        break;
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    
+    case 2:
 
-    return 0;
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero);
+
+       for (i; i <= numero - 1; i++)
+       {
+        printf("Andando 1 para a Direita\n");
+       } 
+       
+        break;
+
+    case 3:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero);
+
+       for (i ; i <= numero - 1; i++)
+       {
+        printf("Andando 1 para Cima\n");
+       }
+       
+        break;
+    
+    default:
+    printf("Opção Inválida \n");
+        break;
+    }
+
+// ===================================================================================================
+
+printf("\n");
+printf("Escolha a direção que deseja ir com o bispo!: \n");
+    printf("1. Diagonal Esquerda\n");
+    printf("2. Diagonal Direita\n");
+   
+    printf("Para qual direção você  vai ?: \n");
+    scanf("%d", &escolhadirecao2);
+    
+    switch (escolhadirecao2)
+    {
+
+    case 1:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero2 );
+
+        while (i <= numero2 + 1 )
+        {
+            printf("Andando 1 casa para a Esquerda e para Cima\n");
+            i++;
+        }
+        break;
+
+    
+    case 2:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero2);
+
+        while (i <= numero2 + 1 )
+        {
+            printf("Andando 1 casa para a Direita e para Cima\n");
+            i++;
+        }
+       
+        break;
+
+    
+    default:
+    printf("Opção Inválida \n");
+        break;
+    }
+
+// ================================================================================================
+    printf("\n");
+    printf("Escolha a direção que deseja ir com a rainha!: \n");
+    printf("1. Esquerda\n");
+    printf("2. Direita\n");
+    printf("3. Cima\n");
+    printf("4. Baixo\n");
+    printf("Para qual direção você  vai ?: \n");
+    scanf("%d", &escolhadirecao3);
+    
+    switch (escolhadirecao3)
+    {
+
+    case 1:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero3);
+
+       do
+       {
+        printf("Avançando 1 Casa para a Esquerda!\n");
+        i++;
+       } while (i +1 <= numero3 + 1);
+       
+       
+        break;
+
+    
+    case 2:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero3);
+
+       do
+       {
+        printf("Avançando 1 Casa para a Direita!\n");
+        i++;
+       } while (i + 1 <= numero3 + 1);
+       
+        break;
+
+    case 3:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero3);
+
+       do
+       {
+        printf("Avançando 1 Casa para Cima!\n");
+        i++;
+       } while (i + 1 <= numero3 + 1);
+       
+        break;
+
+    case 4:
+
+       printf ("Escolha agora a quantidade de Casas: \n");
+       scanf("%d", &numero3);
+
+       do
+       {
+        printf("Avançando 1 Casa para Baixo!\n");
+        i++;
+       } while (i + 1 <= numero3 );
+       
+        break;
+    
+    default:
+    printf("Opção Inválida \n");
+        break;
+    }
+
+
+    
 }
